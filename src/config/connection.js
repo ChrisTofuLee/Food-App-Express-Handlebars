424ed19b5937c46ff83cba66ed721cedbb57d724
Selfay <chris.lee90@hotmail.co.uk>
2020-08-03 00:49:23 +0100

    adding in heroku settings

--- src/config/connection.js
+++ src/config/connection.js
@@ -1,19 +1,34 @@
 const mysql = require("mysql");
 
-const dpOptions = {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Password",
-    database: "foods_db",
-    multipleStatements: true
-}
+let connection;
 
-const connection = mysql.createConnection(dpOptions);
+const productionOptions = {
+  host: process.env.HOSTNAME,
+  port: 3306,
+  dialect: "mysql",
+  use_env_variable: "JAWSDB_URL",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+};
 
 const onConnect = (err) => {
-    if (err) throw err;
-    console.log("Successfully connected to DB");
+  if (err) throw err;
+  console.log("Successfully connected to DB");
+};
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Password",
+        database: "foods_db",
+      });
 }
 
 connection.connect(onConnect);
